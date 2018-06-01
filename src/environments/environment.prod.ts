cf0b5738d83ed0c664a8150d66b7c536299ed29a
Alejandro <alejandrofpo@gmail.com>
2018-06-01 13:21:11 +0200

    Add firebase, auth & firestore services to the app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAPil2za_u2I-a8aln44xFv33djjxSLVss',
+    authDomain: 'angularfirebasegdg.firebaseapp.com',
+    databaseURL: 'https://angularfirebasegdg.firebaseio.com',
+    projectId: 'angularfirebasegdg',
+    storageBucket: 'angularfirebasegdg.appspot.com',
+    messagingSenderId: '26732418254'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAPil2za_u2I-a8aln44xFv33djjxSLVss',
+    authDomain: 'angularfirebasegdg.firebaseapp.com',
+    databaseURL: 'https://angularfirebasegdg.firebaseio.com',
+    projectId: 'angularfirebasegdg',
+    storageBucket: 'angularfirebasegdg.appspot.com',
+    messagingSenderId: '26732418254'
+  }
 };
-
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
