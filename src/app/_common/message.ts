f65bb6ea22fa1a0d7e234258dac15d21a41cd5a1
Alejandro <alejandrofpo@gmail.com>
2018-06-07 19:05:55 +0200

    Update message interface for createdAt to become a type of firebase timestamp

--- src/app/_common/message.ts
+++ src/app/_common/message.ts
@@ -1,6 +1,8 @@
+import * as firebase from 'firebase';
+
 export interface Message {
   userId: string;
   text: string;
   imageURL: string;
-  createdAt: Date;
+  createdAt: firebase.firestore.FieldValue;
 }
